315d63dab9f1baaf6bcd0843b006ace89a6e64af
miguelgonrod <miguelgonrod2004@gmail.com>
2025-05-08 15:57:44 -0500

    Fix: modified servo control to make steps

--- lib/Servomotor/servomotor.cpp
+++ lib/Servomotor/servomotor.cpp
@@ -8,9 +8,17 @@ void Servomotor::initialize(){
   myservo.attach(servo_pin);   
 }
 
-void Servomotor::position(int angle) 
-{                  
-  myservo.write(angle);
+void Servomotor::position(int targetAngle) 
+{
+  int currentAngle = myservo.read();
+  int step = (targetAngle > currentAngle) ? 1 : -1;
+
   Serial.print("writting angle: ");
-  Serial.println(angle);
+  Serial.println(targetAngle);
+
+  while (currentAngle != targetAngle) {
+    currentAngle += step;
+    myservo.write(currentAngle);
+    delay(10);
+  }
 }
